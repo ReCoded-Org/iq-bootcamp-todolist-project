b8479e9885789f48b80f4abda15740be36d5a202
osamaakb <osama98akb@gmail.com>
2020-05-15 17:09:49 +0300

    added v1 (same full but with some lines removed)

--- index.js
+++ index.js
@@ -2,39 +2,22 @@ document.addEventListener('DOMContentLoaded', run);
 
 let dateInstance = M.Datepicker.init(document.querySelectorAll('.datepicker'));
 let tasksList = document.getElementById('tasksList')
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-let addTaskBtn = document.getElementById('addTask')
+let tasks = [];
 let formAddBtn = document.querySelector('form')
 let taskForm = document.getElementById('taskForm')
-let cancelTaskBtn = document.getElementById('cancelTask')
-let prioritiesSelect = document.getElementById('prioritiesSelect');
 let title = document.getElementById('title');
 let deadline = document.getElementById('deadline');
-let doneTaskBtns = document.getElementsByClassName('done-task');
-let deleteTaskBtns = document.getElementsByClassName('delete-task');
 let noTasksText = document.querySelector('.no-tasks');
-
 
 function run() {
 
     initializeMaterialize();
     setTodaysDate();
-    initializePriorities();
     initializeTasks(tasks);
-
-    addTaskBtn.addEventListener('click', function () {
-        taskForm.classList.remove('hidden');
-        addTaskBtn.classList.add('hidden');
-    })
-
-    cancelTaskBtn.addEventListener('click', function () {
-        taskForm.classList.add('hidden');
-        addTaskBtn.classList.remove('hidden');
-    })
 
     formAddBtn.addEventListener('submit', (e) => {
         e.preventDefault();
-        addTask(title.value, prioritiesSelect.options[prioritiesSelect.selectedIndex].value,
+        addTask(title.value,
             new Date, deadline.value)
     });
 }
@@ -49,8 +32,10 @@ function initializeMaterialize() {
 function setTodaysDate() {
     let todaysDate = document.getElementById('todaysDate');
     let date = new Date();
-    curMonth = monthsNames(date.getMonth());
-    curDay = date.getDay();
+    let curMonth = monthsNames(date.getMonth());
+    let curDay = date.getDay();
+    console.log(date);
+
     todaysDate.innerHTML = `<span class="font-bold">Today:</span> ${curMonth} ${curDay}`
 }
 
@@ -59,72 +44,31 @@ function monthsNames(index) {
     return months[index]
 }
 
-function initializePriorities() {
-    let priorities = [1, 2, 3]
-    let prioritiesSelect = document.getElementById('prioritiesSelect');
-
-    priorities.forEach(priority => {
-        prioritiesSelect.insertAdjacentHTML('beforeend', `<option>Priority ${priority}</option>`)
-    })
-}
-
 function initializeTasks(tasks) {
     tasksList.innerHTML = '';
     if (tasks.length !== 0) {
         noTasksText.classList.add('hidden');
         tasks.forEach(task => addTaskToList(task));
-
-        for (let i = 0; i < doneTaskBtns.length; i++) {
-            doneTaskBtns[i].addEventListener('click', (e) => {
-                e.preventDefault();
-                doneTask(i)
-            });
-        }
-
-        for (let index = 0; index < deleteTaskBtns.length; index++) {
-            deleteTaskBtns[index].addEventListener('click', (e) => {
-                e.preventDefault();
-                deleteTask(index);
-            })
-        }
     } else {
         noTasksText.classList.remove('hidden');
     }
 }
 
 function addTaskToList(task) {
-    // let deadlineToDate = new Date(task.deadline.substring(0, 10));
-    // let deadline = `${monthsNames(deadlineToDate.getMonth())} ${deadlineToDate.getDay()}`
-    let priorityColor = getPriorityColor(task.priority);
-    let titleBg = titleBgColor(task.deadline);
-    console.log(titleBg);
-
 
     tasksList.insertAdjacentHTML('beforeend', `
         <li>
             <div class="flex flex-col">
                 <div class="flex flex-row justify-between items-center">
-                    <div class="flex flex-row">
-                        <div class="w-12 h-12 flex items-center justify-center">
-                            <a href="#" class="text-black check done-task">
-                                <i class="far fa-lg fa-circle icon-uncheck"></i>
-                                <i class="far fa-lg fa-check-circle icon-check"></i>
-                            </a>
-                        </div>
+                    <div class="flex flex-row">                        
                         <div>
-                            <p class="text-lg task-title ${titleBg}">${task.taskTitle}</p>
+                            <p class="text-lg task-title">${task.taskTitle}</p>
                             <div class="flex text-gray-600 text-sm">
-                                <p class="p-1">${task.deadline}</p>
-                                <p class="p-1 ${priorityColor}">${task.priority}</p>
+                                <p class="p-1">${task.deadline}</p>                    
                             </div>
                         </div>
                     </div>
-                    <a href="#"
-                        class="flex items-center justify-center hover:bg-gray-200 w-8 h-8 rounded-full text-gray-700 delete-task">
-                        <i class="fas fa-trash"></i>
-                    </a>
-
-                 
+                     
                 </div>
                 <hr class="my-3 ml-5">
             </div>
@@ -132,72 +76,13 @@ function addTaskToList(task) {
     `)
 }
 
-function getPriorityColor(priority) {
-    if (priority === 'Priority 1') {
-        return 'text-red-500';
-    } else if (priority === 'Priority 2') {
-        return 'text-yellow-500';
-    } else {
-        return 'text-green-500';
-    }
-}
-
-function addTask(taskTitle, priority, createdDate, deadline) {
-
-    tasks.push({ taskTitle, priority, createdDate, deadline })
+function addTask(taskTitle, createdDate, deadline) {
+    tasks.push({ taskTitle, createdDate, deadline })
     initializeTasks(tasks);
-    // date is coverted to string too
-    localStorage.setItem('tasks', JSON.stringify(tasks));
     resetInputs();
-}
-
-// postpone this for now
-// function getTaskIndexAfterFiltering(priority, createdDate, deadline) {
-//     // index = 0;
-//     tasks.forEach(task => {
-//         if (task.priority === priority) {
-//             console.log('here priority');
-
-//             if (task.deadline === deadline) {
-//                 console.log('here deadline');
-
-//                 if (new Date(tasks.createdDate) === createdDate) {
-//                     console.log(task.title + 'true');
-//                 }
-//             }
-//         }
-//     })
-// }
-
-function deleteTask(index) {
-    tasks.splice(index, 1)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-    initializeTasks(tasks);
-}
-
-function doneTask(index) {
-    let taskTitles = document.getElementsByClassName('task-title');
-    // Todo make these icons show checked when the task is checked
-    // let iconCheck = document.getElementsByClassName('icon-check');
-    // let iconUncheck = document.getElementsByClassName('icon-uncheck');
-
-    if (taskTitles[index].classList.contains('line-through')) {
-        taskTitles[index].classList.remove('line-through')
-    } else
-        taskTitles[index].classList.add('line-through')
 }
 
 function resetInputs() {
     title.value = '';
     deadline.value = '';
-}
-
-function titleBgColor(deadline) {
-    let deadlineDate = new Date(deadline);
-    let todaysDate = new Date();
-
-    if (todaysDate > deadlineDate)
-        return 'bg-red-200 rounded-lg p-1'
-    else
-        return 'bg-transparent'
 }
